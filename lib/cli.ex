5bb2d8e4948c6fbf5363bdff52babb4385448b4e
= <=>
2019-06-29 18:09:04 -0400

    Adds function to change git remote of repos in a directory

--- lib/cli.ex
+++ lib/cli.ex
@@ -1,6 +1,25 @@
 defmodule CommandLine.CLI do
-  def main(args) do
-    { opts, _, _ } = OptionParser.parse(args, switches: [file: :string], aliases: [f: :file])
-    IO.inspect opts
+  def main([path | _opts]) do
+    path
+    |> String.trim
+    |> list_files
+    |> parse_directories
+    |> add_directories_to_github(path)
+  end
+
+  def list_files(path) do
+    System.cmd("ls", [path])
+  end
+
+  def parse_directories({ directories, _ }) do
+    directories
+    |> String.split("\n")
+  end
+
+  def add_directories_to_github([repo | _the_rest], current_path) do
+    IO.inspect "#{current_path}/#{repo}"
+    File.cd!("#{current_path}/#{repo}", fn () -> 
+      System.cmd("git", ["remote", "set-url", "origin", "git@github.com:programming-notes/#{repo}.git"]) 
+    end)
   end
 end

f496185db75ac68f1c94e178c17b9f68770f59df
= <=>
2019-06-30 10:03:22 -0400

    Adds script to push each subdirectory of a directory to github

--- lib/cli.ex
+++ lib/cli.ex
@@ -16,10 +16,45 @@ defmodule CommandLine.CLI do
     |> String.split("\n")
   end
 
-  def add_directories_to_github([repo | _the_rest], current_path) do
-    File.cd!("#{current_path}/#{repo}", fn () -> 
-      System.cmd("git", ["remote", "set-url", "origin", "git@github.com:programming-notes/#{repo}.git"])
-      System.cmd("git", ["push", "origin", "master"]) 
-    end)
+  def add_directories_to_github([], _current_path) do
+    IO.inspect "Finished"
+  end
+
+  def add_directories_to_github([repo | the_rest], current_path) do
+    task = Task.async fn () ->
+      File.cd!("#{current_path}/#{repo}", fn () ->
+        IO.inspect "Pushing to #{current_path}/#{repo}"
+        repo
+        |> set_remote_url
+        |> create_remote_repo
+        |> push_to_remote
+      end)
+    end
+    Task.await(task)
+    add_directories_to_github(the_rest, current_path)
+  end
+
+  def set_remote_url(repo) do
+    System.cmd("git", ["remote", "set-url", "origin", "git@github.com:programming-notes/#{repo}.git"])
+    repo
+  end
+
+  def push_to_remote(_repo) do
+    System.cmd("git", ["push", "origin", "master"]) 
+  end
+
+  def create_remote_repo(repo) do
+    HTTPoison.post(
+      "https://api.github.com/orgs/programming-notes/repos", 
+      Jason.encode!(%{
+        name: repo,
+        private: false,
+        has_issues: true,
+        has_projects: false,
+        has_wiki: false
+      }),
+      [{"Authorization", "token #{System.get_env("GIT_TOKEN")}"}]
+    )
+    repo
   end
 end

10c7b670acd5156f9bfc18de95a9340cf982e8b9
= <=>
2019-07-01 12:42:59 -0400

    Move github REST API logic to gitAdapter module

--- lib/cli.ex
+++ lib/cli.ex
@@ -20,17 +20,21 @@ defmodule CommandLine.CLI do
     IO.inspect "Finished"
   end
 
+  def add_directories_to_github([""], _current_path) do
+    IO.inspect "Finished"
+  end
+
   def add_directories_to_github([repo | the_rest], current_path) do
-    task = Task.async fn () ->
+    Task.start fn () ->
       File.cd!("#{current_path}/#{repo}", fn () ->
         IO.inspect "Pushing to #{current_path}/#{repo}"
         repo
         |> set_remote_url
-        |> create_remote_repo
+        |> GithubAdapter.create_remote_repo("programming-notes")
         |> push_to_remote
       end)
     end
-    Task.await(task)
+    :timer.sleep(1000)
     add_directories_to_github(the_rest, current_path)
   end
 
@@ -41,20 +45,5 @@ defmodule CommandLine.CLI do
 
   def push_to_remote(_repo) do
     System.cmd("git", ["push", "origin", "master"]) 
-  end
-
-  def create_remote_repo(repo) do
-    HTTPoison.post(
-      "https://api.github.com/orgs/programming-notes/repos", 
-      Jason.encode!(%{
-        name: repo,
-        private: false,
-        has_issues: true,
-        has_projects: false,
-        has_wiki: false
-      }),
-      [{"Authorization", "token #{System.get_env("GIT_TOKEN")}"}]
-    )
-    repo
   end
 end

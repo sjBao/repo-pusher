11330ce17e72a9005d2eb3b5088e6e5ec6bd6140
= <=>
2019-07-01 13:07:29 -0400

    Add ability to specify org_name

--- lib/cli.ex
+++ lib/cli.ex
@@ -1,14 +1,14 @@
 defmodule CommandLine.CLI do
-  def main([path | _opts]) do
-    path
+  def main([repos_dir, org_name | _opts]) do
+    repos_dir
     |> String.trim
     |> list_files
     |> parse_directories
-    |> add_directories_to_github(path)
+    |> add_directories_to_github(repos_dir, org_name)
   end
 
   def list_files(path) do
-    System.cmd("ls", [path])
+    System.cmd("ls", [path, "-1 -d \"$PWD\"*"])
   end
 
   def parse_directories({ directories, _ }) do
@@ -16,34 +16,36 @@ defmodule CommandLine.CLI do
     |> String.split("\n")
   end
 
-  def add_directories_to_github([], _current_path) do
+  def add_directories_to_github([], _, _) do
     IO.inspect "Finished"
   end
 
-  def add_directories_to_github([""], _current_path) do
+  def add_directories_to_github([""], _, _) do
     IO.inspect "Finished"
   end
 
-  def add_directories_to_github([repo | the_rest], current_path) do
+  def add_directories_to_github([repo | the_rest], repos_dir, org_name) do
     Task.start fn () ->
-      File.cd!("#{current_path}/#{repo}", fn () ->
-        IO.inspect "Pushing to #{current_path}/#{repo}"
+      File.cd!("#{repos_dir}/#{repo}", fn () ->
+        IO.inspect "Initating push of #{repos_dir}/#{repo}... to https://github.com/#{org_name}"
         repo
-        |> set_remote_url
-        |> GithubAdapter.create_remote_repo("programming-notes")
+        |> set_remote_url(org_name)
+        |> GithubAdapter.create_remote_repo(org_name)
         |> push_to_remote
       end)
     end
     :timer.sleep(1000)
-    add_directories_to_github(the_rest, current_path)
+    add_directories_to_github(the_rest, repos_dir)
   end
 
-  def set_remote_url(repo) do
-    System.cmd("git", ["remote", "set-url", "origin", "git@github.com:programming-notes/#{repo}.git"])
+
+
+  defp set_remote_url(repo, org_name) do
+    System.cmd("git", ["remote", "set-url", "origin", "git@github.com:#{org_name}/#{repo}.git"])
     repo
   end
 
-  def push_to_remote(_repo) do
+  defp push_to_remote(_repo) do
     System.cmd("git", ["push", "origin", "master"]) 
   end
 end
